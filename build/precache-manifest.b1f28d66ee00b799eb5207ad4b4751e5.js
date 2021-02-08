83b9fa657f68fbe406ad1d8036da2ea205ade2b3
Tomás Catena <tomascatena@gmail.com>
2021-02-08 18:35:41 +0100

    Built for deployment

---
+++ build/precache-manifest.b1f28d66ee00b799eb5207ad4b4751e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4c8bb8be7d837db703a",
+    "url": "/static/css/main.fc65257c.chunk.css"
+  },
+  {
+    "revision": "f4c8bb8be7d837db703a",
+    "url": "/static/js/main.f4c8bb8b.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5acc80e7315d12462b64e6338d656663",
+    "url": "/index.html"
+  }
+];
